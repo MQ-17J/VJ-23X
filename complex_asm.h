6bef57e797ae99e505df510248da74f1b364590c
MQ-17J <forkedrepository@gmail.com>
2026-03-14 22:42:41 -0500

    added complex type

---
+++ complex_asm.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <immintrin.h>
+
+#ifdef __cplusplus
+namespace mq17j
+{
+    extern "C" {
+#endif
+
+        __declspec(align(32)) typedef struct _cscalars64 {
+            double e0, e1;
+        } cscalars64;
+
+        //could do align(32), but keep 64 in case AVX-512 used in edge cases of stacked quaternions
+        __declspec(align(64)) typedef union _complex64 {
+            __m128d avx128;
+            cscalars64 vals;
+        } complex64;
+
+        void complex_norm64_AVX2(const complex64 *c, double *result);
+        void complex_mul64_AVX2(const complex64 *c1, const complex64 *c2, complex64 *result);
+        void complex_div64_AVX2(const complex64 *c1, const complex64 *c2, complex64 *result);
+        void complex_mul64s_AVX2(const complex64 *c, const double *d, complex64 *result); //scalar multiply
+        void complex_div64s_AVX2(const complex64 *c, const double *d, complex64 *result); //scalar division
+        void complex_add64_AVX2(const complex64 *c1, const complex64 *c2, complex64 *result);
+        void complex_sub64_AVX2(const complex64 *c1, const complex64 *c2, complex64 *result);
+
+#ifdef __cplusplus
+    }
+}
+#endif
